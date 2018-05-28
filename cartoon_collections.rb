fa9a92ae8ab5e38f7405cce6009add153162d5c8
Allegra <aukseallegra@gmail.com>
2018-05-28 21:48:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,10 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.include?(cheese_types)
+    
+  end
 end
 
 
